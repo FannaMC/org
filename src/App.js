bbdff1c31d01674b3dc4a42bfa59e2c35be85489
Estefania Muñoz <estefania.mc.1719@gmail.com>
2024-07-11 08:54:48 -0600

    mas cambios header

--- src/App.js
+++ src/App.js
@@ -7,20 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      
     </div>
   );
 }
