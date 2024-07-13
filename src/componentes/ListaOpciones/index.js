bffa81e5be3d6b0436f0a7b634e7d85658f310a1
Estefania Muñoz <estefania.mc.1719@gmail.com>
2024-07-12 21:02:22 -0600

    cambios equipos

--- src/componentes/ListaOpciones/index.js
+++ src/componentes/ListaOpciones/index.js
@@ -7,15 +7,6 @@ const ListaOpciones = (props) => {
     // } )
 
     //esto es como recorrer un arreglo
-    const equipos = [
-        "Programación",
-        "Front End",
-        "Data Science",
-        "Devops",
-        "UX y Diseño",
-        "Móvil",
-        "Innovación y Gestión"
-    ]
 
     const manejarCambio = (e) => {
         console.log("cambio", e.target.value);
@@ -26,7 +17,7 @@ const ListaOpciones = (props) => {
         <label>Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
             <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
-            {equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
+            {props.equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
         </select>
     </div>
 }
